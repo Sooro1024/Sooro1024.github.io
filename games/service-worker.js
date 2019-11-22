aa0ae01e62364d948b05d5c988956dbcce1a05c1
Gor Gevorgyan <soorogorgoyan@gmail.com>
2019-11-22 16:32:06 +0400

    correct an router property for game bundle

--- games/service-worker.js
+++ games/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64cb438008af9f83e528f3754f38a8b3.js"
+  "/games/precache-manifest.1f9903b79a69c1a0143e1a631823edc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/games/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
